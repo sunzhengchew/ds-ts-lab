c4de2f540f49f6af35e558af8c55242f98db5425
sunzhengchew <szcheww@icloud.com>
2026-09-25 00:17:23 +0100

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,9 +1,35 @@
-import type { Friend } from "./myTypes";
-import { friends } from "./01-basics";
+import {Friend, Colleague } from './myTypes'
+import { friends, colleagues } from "./01-basics";
+
 
 function older(f: Friend): string {
   f.age += 1;
   return `${f.name} is now ${f.age}`;
 }
 
-console.log(older(friends[0]));
+function highestExtension(cs: Colleague[]): Colleague {
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
+}
+
+function addColleague(
+  cs: Colleague[],
+  name: string,
+  department: string,
+  email: string,
+  extension: number = 0
+): void {
+  cs.push({
+    name,
+    department,
+    contact: {
+      email,
+      extension,
+    },
+  });
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
